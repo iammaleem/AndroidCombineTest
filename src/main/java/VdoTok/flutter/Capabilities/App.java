7a76ad81fbdb94bf54e43b4dc83c67f7bc81194e
aleem <aleem.asghar@vdotok.com>
2022-12-22 15:10:21 +0500

    hello

---
+++ src/main/java/VdoTok/flutter/Capabilities/App.java
@@ -0,0 +1,13 @@
+package VdoTok.flutter.Capabilities;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
